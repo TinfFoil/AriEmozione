b61d4de46893d40ab110d52912d6a7ebc22f7feb
Daimon <56536141+FrancescoFernicola@users.noreply.github.com>
2020-09-02 14:30:48 +0200

    Update with tokenizer for CNN/RNN

--- Tokenize_Vectorize.py
+++ Tokenize_Vectorize.py
@@ -9,41 +9,38 @@ import it_core_news_sm
 import re
 
 nlp = it_core_news_sm.load()
-tokenized = []
-count = 0
-for verse in aria_text:
-    doc = nlp(verse)
-    tokenized.append([])
-    for w in doc:
-        tokenized[count].append(w.text)
-    count += 1
-# here I was checking that the tokenization was alright, there's just a little bit of noise
-# due to spaces and punctuation
 
-total = [len(x) for x in tokenized]
-total = sorted(total)
-print(total[-5:])
-y = 0
-for x in total:
-    y += x
-avg_len = y / len(total)  # 18
-max_len = 20  # to even it out nicely
 
-for x in aria_text:
-    ' '.join(x.split())
 
 def italian_tokenizer(verse):
-    tokenized = []
+    tokenize = []
     doc = nlp(verse)  # we could add here .casefold() to make it case insensitive
     for w in doc:
         regex = re.compile(r'( +|\'|\-|\,|\!|\:|\;|\?|\.|\(|\)|\«|\»)')
         if not regex.match(w.text):
             w_lower = w.text.casefold()
-            tokenized.append(w_lower)
-    return tokenized
+            tokenize.append(w_lower)
+
+    return tokenize
+
+def tokenizer_FASTTEXT(doc):
+    tokenize = []
+    for x in doc:
+        verse = nlp(x)
+        new_verse = []
+        for w in verse:
+            regex = re.compile(r'( +|\'|\-|\,|\!|\:|\;|\?|\.|\(|\)|\«|\»)')
+            if not regex.match(w.text):
+                w_lower = w.text.casefold()
+                new_verse.append(w_lower)
+        tokenize.append(new_verse)
+
+    return tokenize
+
+tokenized = tokenizer_FASTTEXT(aria_text)
+tokenized_dev = tokenizer_FASTTEXT(dev_text)
 
 sc = StandardScaler()
-
 
 #Character 3-grams - Simple TF-IDF
 vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(3,3), min_df=1)
@@ -51,7 +48,7 @@ trigram_tfidf = vectorizer.fit_transform(raw_documents=aria_text).toarray()
 trigram_tfidf_dev = vectorizer.transform(raw_documents=dev_text).toarray()
 trigram_tfidf = pd.DataFrame(trigram_tfidf)
 trigram_tfidf_dev = pd.DataFrame(trigram_tfidf_dev)
-print('Trigram tfidf shape:', trigram_tfidf.shape)
+
 
 
 #Words - Simple TF-IDF
@@ -63,11 +60,7 @@ dev_tfidf = vectorizer.transform(dev_text).toarray()
 dev_tfidf = pd.DataFrame(dev_tfidf)
 
 
-print(vectorizer.get_feature_names(), f"\nLenght = {len(vectorizer.get_feature_names())}")
-print(vectorizer.vocabulary_, f"\nLenght = {len(vectorizer.vocabulary_)}")
 
-print("This is the Train tfidf:\n", aria_tfidf)
-print("This is the Dev tfidf:\n", dev_tfidf)
 
 
 #create the topic vectors using truncated SVD
@@ -75,14 +68,14 @@ svd = TruncatedSVD(n_components=32, n_iter=100)
 aria_svd = svd.fit_transform(aria_tfidf)
 aria_svd = pd.DataFrame(aria_svd)
 aria_svd = sc.fit_transform(aria_svd)
-print("This is the Train SVD:\n", aria_svd, f"\nShape: {aria_svd.shape}")
+
 
 
 
 dev_svd = svd.transform(dev_tfidf)
 dev_svd = pd.DataFrame(dev_svd)
 dev_svd = sc.fit_transform(dev_svd)
-print("This is the Dev SVD:\n", dev_svd, f"\nShape: {dev_svd.shape}")
+
 
 
 #Character 3-grams - TruncatedSVD Topic Vectors
@@ -180,3 +173,12 @@ def n_grams_generate(text, text_dev, n): #To be discussed and fixed to include b
 
 clean_tfidf, clean_svd, clean_ldia, clean_tfidf_dev, clean_svd_dev, clean_ldia_dev, clean_dims = n_grams_generate(aria_text, dev_text, 3)
 dimensions = trigram_tfidf.shape[1]
+
+if __name__ == '__main__':
+    print(vectorizer.get_feature_names(), f"\nLenght = {len(vectorizer.get_feature_names())}")
+    print(vectorizer.vocabulary_, f"\nLenght = {len(vectorizer.vocabulary_)}")
+    print('Trigram tfidf shape:', trigram_tfidf.shape)
+    print("This is the Train tfidf:\n", aria_tfidf)
+    print("This is the Dev tfidf:\n", dev_tfidf)
+    print("This is the Train SVD:\n", aria_svd, f"\nShape: {aria_svd.shape}")
+    print("This is the Dev SVD:\n", dev_svd, f"\nShape: {dev_svd.shape}")
