b9a833695c0eedc514f239e6e0eb9569435492fe
Daimon <56536141+FrancescoFernicola@users.noreply.github.com>
2020-09-04 09:45:14 +0200

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -44,9 +44,3 @@ encoded_dev = encoder.transform(dev_emotion_list)
 dummy_dev = np_utils.to_categorical(encoded_dev)
 print(f"Classes to be predicted: {encoder.classes_}\n", f"Corresponding number: {set(encoded_dev)}\n",
       f"Instances per class: {Counter(encoded_dev)}")
-
-
-
-#for idx, cell in aria_text.iteritems(): #ZAP1596906_00,ZAP1596906_01 must be corrected, they have no "Emotion" evaluation
-#  if 'Nessuna' in df.loc[idx, 'Emotion']: #skipping lines with no label for Emotion
-#      print('"',df.loc[idx, 'Text'],'"', 'Number of characters: {}'.format(len(df.loc[idx, 'Text'])))
